85cb629aa4c3ce8e7bd55d27892a50c3e662724b
Gabriel Jordan <gabrijordan3@gmail.com>
2025-10-22 17:00:33 -0300

    fix social links

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,7 @@ import { Instagram, Twitch } from "lucide-react";
 import { TbBrandDiscord } from "react-icons/tb";
 
 export const SOCIAL_LINKS = {
-    discord: 	{
+    instagram: 	{
 		name: "Instagram",
 		href: "https://www.instagram.com/bar_dos_amigos_online",
 		icon: Instagram,
@@ -12,7 +12,7 @@ export const SOCIAL_LINKS = {
 		href: "https://www.twitch.tv/kauso1",
 		icon: Twitch,
 	},
-    instagram: 	{
+    discord: 	{
 		name: "Discord",
 		href: "https://discord.bardosamigos.com.br",
 		icon: TbBrandDiscord,
